6904966e315dcbe0989ed784b3dfb60e542cce0f
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-14 01:20:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,7 @@
 def oxford_comma(array)
-# needs to return a string of the values, adding "and" before the last index
-  # make an array from the last element
   last_element = array.pop.split(' ')
   string2 = last_element.unshift(", and").join(" ")
-  # => ", and kohlrabi"
-
   string1 = array.join(", ")
-  # => "fiddleheads, okra"
-
   final_string = string1 + string2
 
-  # returns "fiddleheads, okra, and kohlrabi"
 end
